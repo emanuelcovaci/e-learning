415f11ee4dcb30ccc097c696550182bc3217e979
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-11-01 20:24:00 +0200

    index page works

--- e_learning/e_learning/settings.py
+++ e_learning/e_learning/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'homepage',
+    'homepages',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
