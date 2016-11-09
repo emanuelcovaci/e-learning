9ffd1fe603c83d67bccfa60bf9cff4916f765cb7
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-11-09 20:56:55 +0200

    domain in home page models

--- e_learning/homepages/views.py
+++ e_learning/homepages/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render_to_response
 
 # Create your views here.
+from domain.models import Domain
 
 
 def home(request):
+    domain = Domain.objects.all()
     if request.user.is_authenticated():
         template = 'homepages/home.html'
     else:
         template = 'homepages/index.html'
     return render_to_response(template,{
             'user': request.user,
+             'domain':domain,
             })
